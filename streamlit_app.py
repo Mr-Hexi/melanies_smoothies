9831bc7372e07b13201583c59dd0e6ecbbbe105b
Huzaifa <abuhuzaifakurla@gmail.com>
2026-05-15 16:38:35 +0530

    removed pandas table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(data=pd_df)
-st.stop()
+# st.dataframe(data=pd_df)
+# st.stop()
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients:",
         my_dataframe,
@@ -41,7 +41,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        
+
         st.subheader(fruits_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
